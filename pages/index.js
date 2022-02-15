9d65bb5c446ce703179db2eb09b9fca7d81ea3cb
blackhallhack <69937680+blackhallhack@users.noreply.github.com>
2022-02-15 22:17:41 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,28 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>핵메타 배그핵 서든핵 옵치핵 최저가 판매</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+     <head>
+    <title>핵메타 배그핵 서든핵 옵치핵 최저가 판매중</title>
+    <meta name="description" content="국내 최저가 배그핵 서든핵 옵치핵 등 다양한 제품 최대보유중 정지없는 랭커용 방송용 슬롯제 최대할인 이벤트 24시간 고객센터 운영중" />
+    <meta name="keywords" content="핵메타 / 메타핵 / 서든메타 / 배그메타 / 배그핵 / 배그 핵 / 서든핵 /서든 핵 / 오레오 / 배그핵 디스코드 / 배틀그라운드 핵 /배틀그라운드핵 /서든어택핵 /서든어택 핵 / 배그 ESP / 서든 월핵 / 배그핵 전문 너지몰 / 배그핵 메타몰 / 서든핵 메타몰 / 서든어택 핵 너지몰 / 배틀그라운드 핵 너지몰 / 배틀그라운드 슬롯제 / 배그 슬롯제 / PUBG AIMBOT / 배그 에임봇 / 서든 오토에임 / 서든 월핵 / 배그 핵 사이트 / 배그 핵 구매 사이트 / 배그 핵 판매사이트 / 배틀그라운드 핵 구매 / 배그 핵 판매 / 서든어택 계정샵 / 서든 계정샵 / 핵판매샵 / 서든핵판매사이트 / 서든핵판매샵 / 배그 핵 구매 디스코드 / 배그핵 사이트 / 배그 esp" />
+    <meta name="author" content="HACKMETA" />
+    <link rel="canonical" href="https://hackmarket.netlify.app/" />
+    <meta name="robots" content="index, follow" />
+    
+    <!--  Essential Social Meta Tags -->
+    <meta property="og:title" content="핵메타 배그핵 서든핵 옵치핵 최저가 판매중" />
+    <meta property="og:description" content="국내 최저가 배그핵 서든핵 옵치핵 등 다양한 제품 최대보유중 정지없는 랭커용 방송용 슬롯제 최대할인 이벤트 24시간 고객센터 운영중" />
+    <meta property="og:image" content="https://media.discordapp.net/attachments/768640957725016066/931715980390981682/meta-logo2.jpg?width=499&height=468" />
+    <meta property="og:url" content="https://hackmarket.netlify.app/" />
+    <meta property="twitter:card" content="https://media.discordapp.net/attachments/768640957725016066/931715980390981682/meta-logo2.jpg?width=499&height=468" />
+    <meta property="og:site_name" content="핵메타 배그핵 서든핵 옵치핵 최저가 판매중" />
+    <meta property="twitter:image:alt" content="핵메타 배그핵 서든핵 옵치핵 최저가 판매중" />
+</head>
 
       <main>
-        <Header title="시장점유율 1위 핵메타" />
+        <Header title="핵메타 배그핵 서든핵 옵치핵 국내최저가 판매중" />
         <p className="description">
-          사이트 바로가기<code>핵메타.com</code>
+         <button onclick="window.location.href='https://xn--h32b232bikd.com'">핵메타 바로가기</button>
         </p>
       </main>
 
